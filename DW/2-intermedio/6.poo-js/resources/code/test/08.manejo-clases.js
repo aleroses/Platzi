40e67faa3efe8c5ca7f8435d9e8bc01fc71ea443
ale vel <ale.vrs@outlook.com>
2023-12-23 22:20:56 -0500

    Update

---
+++ DW/2-intermedio/6.poo-js/resources/code/test/08.manejo-clases.js
@@ -0,0 +1,45 @@
+class Band {
+  constructor({ name, genre, members = [] }) {
+    this.name = name;
+    this.genre = genre;
+    this.members = members;
+  }
+
+  add_member(new_member) {
+    this.members.push(new_member);
+  }
+}
+
+class Member {
+  constructor({ name, instrument }) {
+    this.name = name;
+    this.instrument = instrument;
+  }
+}
+
+const member_one = new Member({
+  name: "Simone Simons",
+  instrument: "Singer",
+});
+
+const member_two = new Member({
+  name: "Alissa White-Gluz",
+  instrument: "Drums",
+});
+
+const member_three = new Member({
+  name: "Angela Gossow",
+  instrument: "Drums",
+});
+
+const band_one = new Band({
+  name: "Misfits",
+  genre: "Punk",
+  // members: [member_one],
+});
+
+band_one.add_member(member_one);
+band_one.add_member(member_two);
+band_one.add_member(member_three);
+
+console.log(band_one);
