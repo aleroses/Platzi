fbe43820f3ef2611bb3ec76cbe98fbe7bea5cb18
ale vel <ale.vrs@outlook.com>
2023-12-27 17:20:48 -0500

    Update

---
+++ DW/2-intermedio/6.poo-js/resources/code/test/10.abstraccion.js
@@ -0,0 +1,5 @@
+// Prototipo para Schools
+// 1. Crear una clase para escuelas (Learnin_path())
+// 2. Crear una instancia para cada escuela (escuela_dw) Pupil
+// 3. Revisar que no se repitan coursos dentro de las escuelas
+
