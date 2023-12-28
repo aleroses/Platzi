0aa11a122f69adf6da105341a54c2b9e3dfc6d42
ale vel <ale.vrs@outlook.com>
2023-12-28 18:48:46 -0500

    Update

---
+++ DW/2-intermedio/6.poo-js/resources/code/test/14.playgrounds.mjs
@@ -0,0 +1,32 @@
+import { Course } from "./14.playgrounds-get-set.mjs";
+
+const course_one = new Course({
+  name: "Course One",
+  classes: [
+    "Class one",
+    "Class two",
+    "Class three",
+  ],
+});
+
+course_one.name = " secret course ";
+
+// console.log(course_one.name);
+console.log(course_one._name);
+
+/* import { Course } from "./14.playgrounds-get-set.mjs";
+
+const course_one = new Course({
+  name: "Course one",
+  classes: [
+    "Topic one",
+    "Topic two",
+    "Topic three",
+  ],
+});
+
+course_one.name = 123;
+//course_one.name = " secret course ";
+
+// console.log(course_one);
+console.log("Current name: ", course_one._name); */
