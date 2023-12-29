95371b3434c207c4768dc1755d2d4fa193f46909
ale vel <ale.vrs@outlook.com>
2023-12-28 20:17:53 -0500

    Update

--- DW/2-intermedio/6.poo-js/resources/code/15.inheritance.js
+++ DW/2-intermedio/6.poo-js/resources/code/15.inheritance.js
@@ -10,14 +10,44 @@ class Animal {
 
 class Dog extends Animal {
   constructor({ name, breed }) {
-    super(name);
+    super({ name });
     this.breed = breed;
   }
 
   speak() {
+    super.speak();
     console.log(`${this.name} barks!!`);
   }
 }
 
-const dog = new Dog({ name: "Fido" });
+const dog = new Dog({
+  name: "Fido",
+  breed: "something",
+});
+
 console.log(dog.speak());
+
+/* class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} hace un sonido.`);
+  }
+}
+
+class Perro extends Animal {
+  constructor(name, raza) {
+    super(name);
+    this.raza = raza;
+  }
+
+  speak() {
+    super.speak();
+    console.log(`${this.name} ladra.`);
+  }
+}
+
+let perro = new Perro("Fido", "Golden Retriever");
+perro.speak(); // "Fido hace un sonido." "Fido ladra." */
