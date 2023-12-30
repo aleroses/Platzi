42745210b243b2dbdcfa90cb21da870bb580bc27
ale vel <ale.vrs@outlook.com>
2023-12-30 17:40:31 -0500

    Update

--- DW/2-intermedio/6.poo-js/resources/code/test/18.poli.js
+++ DW/2-intermedio/6.poo-js/resources/code/test/18.poli.js
@@ -19,3 +19,37 @@ class Comment {
   }
 }
 
+class Pupil {
+  constructor({
+    name,
+    email,
+    username,
+    twitter,
+    instagram,
+    facebook,
+    approved_courses = [],
+    learning_paths = [],
+  }) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.social_media = {
+      twitter,
+      instagram,
+      facebook,
+    };
+    this.approved_courses = approved_courses;
+    this.learning_paths = learning_paths;
+  }
+
+  approved_course(new_course) {
+    this.approved_courses.push(new_course);
+  }
+
+  post_comment(comment_content) {
+    const comment = new Comment({
+      content: comment_content,
+      pupil_name: this.name,
+    });
+  }
+}
