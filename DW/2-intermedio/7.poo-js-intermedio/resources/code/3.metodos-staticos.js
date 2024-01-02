64916ea59a1723ad0959dcf343690ad5ed67d0b7
ale vel <ale.vrs@outlook.com>
2024-01-02 17:06:11 -0500

    Update

---
+++ DW/2-intermedio/7.poo-js-intermedio/resources/code/3.metodos-staticos.js
@@ -0,0 +1,13 @@
+const student = {
+  name: "Ale",
+  age: 18,
+  approvedCourses: ["Course one"],
+
+  addCourse(new_course) {
+    this.approvedCourses.push(new_course);
+  },
+};
+
+console.log(Object.keys(student));
+console.log(Object.getOwnPropertyNames(student));
+console.log(Object.entries(student));
