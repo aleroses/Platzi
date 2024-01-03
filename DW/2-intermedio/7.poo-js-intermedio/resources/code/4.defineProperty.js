b25431a1baea33870086f419c302c69683d2d341
ale vel <ale.vrs@outlook.com>
2024-01-02 20:22:06 -0500

    Update

--- DW/2-intermedio/7.poo-js-intermedio/resources/code/4.defineProperty.js
+++ DW/2-intermedio/7.poo-js-intermedio/resources/code/4.defineProperty.js
@@ -12,3 +12,40 @@ const student = {
     this.approvedCourses.push(new_course);
   },
 };
+
+/* Object.defineProperty(student, "test_01", {
+  value: "OSNI",
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
+
+Object.defineProperty(student, "test_02", {
+  value: "OVNI",
+  writable: false,
+  enumerable: true,
+  configurable: true,
+});
+
+Object.defineProperty(student, "test_03", {
+  value: "X-Files",
+  writable: true,
+  enumerable: true,
+  configurable: false,
+});
+
+Object.defineProperty(student, "test_04", {
+  value: "UFOs",
+  writable: false,
+  enumerable: false,
+  configurable: false,
+}); */
+
+
+// console.log(Object.seal(student));
+Object.freeze(student);
+//console.log(student);
+
+console.log(
+  Object.getOwnPropertyDescriptors(student)
+);
