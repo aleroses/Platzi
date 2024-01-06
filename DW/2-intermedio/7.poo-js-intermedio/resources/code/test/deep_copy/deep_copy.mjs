499f4802d66d2ebd439cff1b7f2b8d19230808e5
ale vel <ale.vrs@outlook.com>
2024-01-05 22:58:37 -0500

    Update

--- DW/2-intermedio/7.poo-js-intermedio/resources/code/test/deep_copy/deep_copy.mjs
+++ DW/2-intermedio/7.poo-js-intermedio/resources/code/test/deep_copy/deep_copy.mjs
@@ -1,17 +1,20 @@
-import { is_object, is_array } from "./validation.mjs";
+import {
+  is_object,
+  is_array,
+} from "./validation.mjs";
 import { object_one, object_two } from "./data.mjs";
 
 function deep_copy(data) {
   // Declarar
-  let copy_data;
+  let data_copy;
 
   const data_is_array = is_array(data);
   const data_is_object = is_object(data);
 
   if (data_is_array) {
-    copy_data = [];
+    data_copy = [];
   } else if (data_is_object) {
-    copy_data = {};
+    data_copy = {};
   } else {
     return data;
   }
@@ -20,17 +23,17 @@ function deep_copy(data) {
     const key_is_object = is_object(data[key]);
 
     if (key_is_object) {
-      copy_data[key] = deep_copy(data[key]);
+      data_copy[key] = deep_copy(data[key]);
     } else {
       if (data_is_array) {
-        copy_data.push(data[key]);
+        data_copy.push(data[key]);
       } else {
-        copy_data[key] = data[key];
+        data_copy[key] = data[key];
       }
     }
   }
 
-  return copy_data;
+  return data_copy;
 }
 
 // No es lo mismo hacer un cambio antes de hacer la copia que..
