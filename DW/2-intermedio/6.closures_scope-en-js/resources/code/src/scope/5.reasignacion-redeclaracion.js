bfb09031cabbbb0477a479136282472b5fbfede6
ale vel <ale.vrs@outlook.com>
2024-01-10 17:46:59 -0500

    Update

---
+++ DW/2-intermedio/6.closures_scope-en-js/resources/code/src/scope/5.reasignacion-redeclaracion.js
@@ -0,0 +1,37 @@
+// Declarar undefined
+var firstName;
+firstName = "Ale";
+console.log(firstName);
+
+// Declarar y asignar o inicializar
+var lastName = "Roses";
+// Reasignar o Reinicialzando
+lastName = "Vel";
+console.log(lastName);
+
+// Declarando e Asignando o inicializando
+var secondName = "X";
+// Redeclarando y Reasignar o Reinicializando
+var secondName = "Y";
+console.log(secondName);
+
+// LET
+let fruit = "Apple";
+fruit = "kiwi";
+console.log(fruit);
+
+//let fruit = "Banana";
+
+// CONST
+const animal = "dog";
+// animal = "cat";
+// const animal = "Cat";
+
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("🏎");
+console.log(vehicles);
+
+vehicles.pop("🏎");
+console.log(vehicles);
