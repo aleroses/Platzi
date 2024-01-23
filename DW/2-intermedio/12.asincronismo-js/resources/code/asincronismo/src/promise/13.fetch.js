288c95938beeefffa653f8c59fab55812e71952d
ale vel <ale.vrs@outlook.com>
2024-01-22 21:01:08 -0500

    Update

--- DW/2-intermedio/12.asincronismo-js/resources/code/asincronismo/src/promise/13.fetch.js
+++ DW/2-intermedio/12.asincronismo-js/resources/code/asincronismo/src/promise/13.fetch.js
@@ -1,5 +1,21 @@
 // npm i node-fetch
 
+/* import fetch from "node-fetch";
+const API = "https://api.escuelajs.co/api/v1";
+
+function fetchData(urlApi) {
+  return fetch(urlApi);
+}
+
+fetchData(`${API}/products`)
+  .then(response => response.json())
+  .then(products => {
+    console.log(products);
+  })
+  .catch(e => {
+    console.log(e);
+  }); */
+
 import fetch from "node-fetch";
 const API = "https://api.escuelajs.co/api/v1";
 
@@ -8,10 +24,24 @@ function fetchData(urlApi) {
 }
 
 fetchData(`${API}/products`)
-  .then(response => response.json)
+  .then(response => response.json())
   .then(products => {
     console.log(products);
+
+    return fetchData(
+      `${API}/products/${products[0].id}`
+    );
   })
-  .catch(e => {
-    console.log(e);
-  });
+  .then(response => response.json())
+  .then(product => {
+    console.log(product.title);
+    return fetchData(
+      `${API}/categories/${product.category.id}`
+    );
+  })
+  .then(response => response.json())
+  .then(category => {
+    console.log(category.name);
+  })
+  .catch(e => console.log(e))
+  .finally(() => console.log("Finally"));
