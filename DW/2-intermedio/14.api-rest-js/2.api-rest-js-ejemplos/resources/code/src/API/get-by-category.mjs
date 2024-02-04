71187e1d83506298e201312db0a058f3eaca303a
ale vel <ale.vrs@outlook.com>
2024-02-04 00:20:48 -0500

    Update

--- DW/2-intermedio/14.api-rest-js/2.api-rest-js-ejemplos/resources/code/src/API/get-by-category.mjs
+++ DW/2-intermedio/14.api-rest-js/2.api-rest-js-ejemplos/resources/code/src/API/get-by-category.mjs
@@ -1,22 +1,5 @@
 import { authorization } from "../parameters.mjs";
 
-/* const getTrendingMovies = async () => {
-  const response = await fetch(
-    "https://api.themoviedb.org/3/trending/all/day?language=en-US",
-    {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        authorization,
-      },
-    }
-  );
-
-  const data = await response.json();
-
-  return data;
-}; */
-
 const movies = ["?", "with_genres="].join("");
 
 const getByCategory = async id => {
